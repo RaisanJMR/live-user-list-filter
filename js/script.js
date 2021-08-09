e00604107522df15074873d8bea67a1bed4545f2
RaisanJMR <raisanjmr@protonmail.com>
2021-08-09 19:09:36 +0530

    made responsive

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ filter.addEventListener('input', (e) => filterData(e.target.value));
 async function getData() {
   const res = await fetch('https://randomuser.me/api/?results=50');
   const { results } = await res.json();
-console.log(results)
+// console.log(results)
   result.innerHTML = '';
   results.forEach((user) => {
     const li = document.createElement('li');
